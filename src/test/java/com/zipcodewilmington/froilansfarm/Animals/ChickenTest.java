b889021572cc98a6a29764c4a4d7b2edae10b377
donnasvinis <dvsvinis@mac.com>
2019-06-23 16:33:59 -0400

    Chicken fertilized Tests completed

---
+++ src/test/java/com/zipcodewilmington/froilansfarm/Animals/ChickenTest.java
@@ -0,0 +1,45 @@
+package com.zipcodewilmington.froilansfarm.Animals;
+
+import com.zipcodewilmington.froilansfarm.EdibleObjects.Egg;
+import com.zipcodewilmington.froilansfarm.Interfaces.EdibleObject;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ChickenTest {
+
+    @Test
+    public void makeNoise() {
+        Chicken chicken = new Chicken();
+        //when
+        String expected = "B'Gock!";
+        String actual = chicken.makeNoise();
+        //assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void notFertilizedTest() {
+        Chicken chicken = new Chicken();
+        //when
+        chicken.setHasBeenFertilized(false);
+        //assert
+        Assert.assertNotNull(chicken.Yield());
+    }
+
+    @Test
+    public void fertilizedTest() {
+        Chicken chicken = new Chicken();
+        //when
+        chicken.setHasBeenFertilized(true);
+        //assert
+        Assert.assertNull(chicken.Yield());
+    }
+
+
+
+    @Test
+    public void eat() {
+    }
+}
