c9d34ceab1fea28d86ba583060fabc0b0a3c6ff3
Abram Phillips <abramcpg@hotmail.com>
2019-06-24 16:12:08 -0400

    Started FarmTour class

--- src/main/java/com/zipcodewilmington/froilansfarm/FarmTour.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/FarmTour.java
@@ -8,6 +8,8 @@ import com.zipcodewilmington.froilansfarm.Crops.Field;
 import com.zipcodewilmington.froilansfarm.Interfaces.EdibleObject;
 import com.zipcodewilmington.froilansfarm.Interfaces.FarmVehicle;
 import com.zipcodewilmington.froilansfarm.StoreHouses.Barn;
+import com.zipcodewilmington.froilansfarm.Farm.DayOfWeek;
+import com.zipcodewilmington.froilansfarm.Farm.FroilanFarm.FarmWeek;
 
 import java.util.Scanner;
 
@@ -16,39 +18,30 @@ import static java.lang.System.*;
 public class FarmTour {
 
     private String day = "Sunday";
-    private String time = "Sunrise";
     private Scanner input = new Scanner(System.in);
     private String setTourMessage = String.format(
-            "Howdy! It's % at %. What would you like to see from the options on our pamphlet?\n" +
+            "Oh right! I've been doing so much coding, my days are running together.\n" +
+                    "What would you like to see from the options below?\n" +
             "\t1. Enjoy the sounds of the farm\n" +
             "\t2. Visit the food store houses\n" +
             "\t3. Check out our animals\n" +
             "\t4. Do a tax audit!\n" +
             "\t5. Ride our horses");
-    String tour = "1";
+    private String tour = "1";
+
 
     public void init(){
         setDay();
-        setTime();
+        DayOfWeek.valueOf(day);
         setTour();
-        //tour
+        //callTour
+
     }
-
-
-
-
-
 
     public void setDay(){
         //Scanner input = new Scanner(System.in); //Create scanner object
-        out.println("Welcome to Froilan's Farm Tour Center!\nWhat day would you like to visit?");
+        out.println("Welcome to Froilan's Farm!\nDo you happen to know what day it is?");
         day = input.nextLine();
-    }
-
-    public void setTime(){
-        out.println("Okay, we have sunrise and sunset tours on " + day + ".\nWhich do you prefer?");
-        time = input.nextLine();
-        out.println("So " + day + " for the " + time + " tour.\n See you there!");
     }
 
     public void setTour(){
@@ -56,6 +49,20 @@ public class FarmTour {
         tour = input.nextLine();
     }
 
+    public void callTour(){
+        switch (tour){
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":
+                break;
+            case "5":
+                break;
+        }
+    }
 
 
 
